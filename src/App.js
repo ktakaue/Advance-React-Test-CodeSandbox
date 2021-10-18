98b3297c0b29168fe9a39ed10469b45d71cb1751
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-18 21:26:05 +0900

    再レンダリングを行うことによるページ遷移への影響 配列内の2000のデータが、文字が1文字入力されるごとに再レンダリングされるのでかなり重たくなる(codesandbox内ももったりしている)

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./styles.css";
 export default function App() {
   console.log("App");
   const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
   const onChangeText = (e) => setText(e.target.value);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
-      <button> 表示</button>
-      <ChildArea />
+      <br />
+      <br />
+      <button onClick={onClickOpen}> 表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,7 +1,26 @@
-export const ChildArea = () => {
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+  console.log("ChildAreaがレンダリングされた！！");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  console.log(data);
+
   return (
-    <div>
-      <p>子コンポーネント</p>
-    </div>
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
   );
 };
